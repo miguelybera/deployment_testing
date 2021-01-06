430f32a09b408c4ef854a3ad764bafe7511a892b
miguelybera <2018115528@ust-ics.mygbiz.com>
2021-01-06 18:45:01 +0800

    first commit
    first commit of website

---
+++ backend/models/product.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter product name'],
+        trim: true,
+        maxlength: [100, 'Product name cannot exceed 100 characters']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter product description'],
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, 'Please select category for this product'],
+        enum: {
+            values: [
+                'Category1',
+                'Category2',
+                'Category3',
+                'Category4',
+                'Category5',
+                'Category6',
+                'Category7',
+            ]
+        }
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema);

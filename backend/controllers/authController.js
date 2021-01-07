443377d52b65e9a717762bd6af193c6d7c6c7179
miguelybera <2018115528@ust-ics.mygbiz.com>
2021-01-07 18:05:29 +0800

    6:05 Jan 7

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 // Register a user => /api/v1/register
 
@@ -19,12 +20,7 @@ exports.registerUser = catchAsyncErrors( async(req, res, next) => {
         }
     })
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    })
+    sendToken(user, 200, res)
 })
 
 // Login User => /api/v1/login
@@ -51,11 +47,7 @@ exports.loginUser = catchAsyncErrors( async(req, res, next) => {
         return next(new ErrorHandler('Invalid Email or Password', 401));
     }
 
-    const token = user.getJwtToken();
-    res.status(200).json({
-        success: true,
-        token
-    })
+    sendToken(user, 200, res)
 
 })
 

58b7bdc91827d0150006d0fe29802eb890844be2
fromabby <60252891+fromabby@users.noreply.github.com>
2021-01-13 18:52:29 +0800

    image working

--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -5,6 +5,7 @@ import { getProductDetails, clearErrors } from '../../actions/productActions'
 import Loader from '../layout/Loader'
 import { useAlert } from 'react-alert'
 import MetaData from '../layout/MetaData'
+import {Carousel} from 'react-bootstrap'
 
 const ProductDetails = ( { match }) => {
 
@@ -32,7 +33,15 @@ const ProductDetails = ( { match }) => {
                     <MetaData title={`${product.name}`}/>
                     <section className="individual-product-section">
                         <div className="row individual-products">
-                            <div className="col-sm-5 col-md-5 col-lg-5 offset-lg-0"><img className="product-image" src={product.images[0].url} /></div>
+                            <div className="col-sm-5 col-md-5 col-lg-5 offset-lg-0">
+                                <Carousel pause='hover'>
+                                    {product.images && product.images.map( image => (
+                                        <Carousel.Item key={image.public_id}>
+                                            <img className="product-image" src={image.url} alt={`image of ${product.name}`}/>
+                                        </Carousel.Item>
+                                    ))}
+                                </Carousel>
+                            </div>
                             <div className="col-sm-7 col-md-7 col-lg-7 offset-lg-0">
                                 <h3 className="individual-product-name">{product.name}</h3><a className="link-back" href="/our-products">Go back&nbsp;<i className="fa fa-angle-right"></i>&nbsp;</a>
                             </div>

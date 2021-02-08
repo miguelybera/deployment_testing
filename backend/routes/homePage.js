0a5b592174d55820c448b1d32c6ed884b72d9c7f
miguelybera <josemiguel.ybera.iics@ust.edu.ph>
2021-02-08 11:07:38 +0800

    new commit with authorize roles

--- backend/routes/homePage.js
+++ backend/routes/homePage.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router();
 
-const{ getHomePage, updateHomePage } = require('../controllers/homePageController')
+const{ getHomePage, updateHomePage } = require('../controllers/homePageController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/homePage').get(getHomePage);
-router.route('/homePage/:id').put(updateHomePage);
+router.route('/admin/homePage/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateHomePage);
 
 
 

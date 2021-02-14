6e047a250048f2d6825945eeb5ec94a17de1e574
miguelybera <josemiguel.ybera.iics@ust.edu.ph>
2021-02-14 20:32:08 +0800

    update on about us backend

--- backend/controllers/aboutController.js
+++ backend/controllers/aboutController.js
@@ -35,6 +35,89 @@ exports.getSingleAbout = catchAsyncErrors (async(req,res,next) => {
     })
 })
 
+// get single about us details => /api/v1/aboutcompany
+exports.getAboutCompany= catchAsyncErrors (async(req,res,next) => {
+    const about = await About.findById('602905af513578177431461d');
+
+    if(!about){
+        return next(new ErrorHandler('About us details Not Found', 404));
+    }
+
+    res.status(200).json({
+        success:true,
+        about
+    })
+})
+
+// get single about us details => /api/v1/aboutobjectives
+exports.getAboutObjectives= catchAsyncErrors (async(req,res,next) => {
+    const about = await About.findById('602905c9513578177431461e');
+
+    if(!about){
+        return next(new ErrorHandler('About us details Not Found', 404));
+    }
+
+    res.status(200).json({
+        success:true,
+        about
+    })
+})
+
+// get single about us details => /api/v1/aboutscope
+exports.getAboutScope= catchAsyncErrors (async(req,res,next) => {
+    const about = await About.findById('602905d6513578177431461f');
+
+    if(!about){
+        return next(new ErrorHandler('About us details Not Found', 404));
+    }
+
+    res.status(200).json({
+        success:true,
+        about
+    })
+})
+
+// get single about us details => /api/v1/aboutmission
+exports.getAboutMission= catchAsyncErrors (async(req,res,next) => {
+    const about = await About.findById('602905e55135781774314620');
+
+    if(!about){
+        return next(new ErrorHandler('About us details Not Found', 404));
+    }
+
+    res.status(200).json({
+        success:true,
+        about
+    })
+})
+// get single about us details => /api/v1/aboutvision
+exports.getAboutVision= catchAsyncErrors (async(req,res,next) => {
+    const about = await About.findById('602905ed5135781774314621');
+
+    if(!about){
+        return next(new ErrorHandler('About us details Not Found', 404));
+    }
+
+    res.status(200).json({
+        success:true,
+        about
+    })
+})
+
+// get single about us details => /api/v1/abouthistory
+exports.getAboutHistory= catchAsyncErrors (async(req,res,next) => {
+    const about = await About.findById('602905f65135781774314622');
+
+    if(!about){
+        return next(new ErrorHandler('About us details Not Found', 404));
+    }
+
+    res.status(200).json({
+        success:true,
+        about
+    })
+})
+
 // update single about us details => /api/v1/about/:id
 
 exports.updateAbout = catchAsyncErrors (async( req, res, next) =>{
--- backend/routes/about.js
+++ backend/routes/about.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const router = express.Router();
 
-const{ newAbout, getAllAbout, getSingleAbout, updateAbout } = require('../controllers/aboutController');
+const{ newAbout, getAllAbout, getSingleAbout, updateAbout, getAboutCompany,getAboutObjectives,getAboutScope,getAboutMission, 
+getAboutVision, getAboutHistory } = require('../controllers/aboutController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/newabout').post(newAbout);
 router.route('/allaboutus').get(getAllAbout);
 router.route('/about/:id').get(getSingleAbout);
 router.route('/admin/about/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateAbout);
+
+// get single parts of about us
+router.route('/aboutcompany').get(getAboutCompany);
+router.route('/aboutobjectives').get(getAboutObjectives);
+router.route('/aboutscope').get(getAboutScope);
+router.route('/aboutmission').get(getAboutMission);
+router.route('/aboutvision').get(getAboutVision);
+router.route('/abouthistory').get(getAboutHistory);
+
 
 module.exports = router
